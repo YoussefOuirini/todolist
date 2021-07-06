7e5fe83c308a5e56e36997c02fb014f1cc742c63
Youssef Ouirini <youssefouirini@gmail.com>
2021-07-06 21:03:21 +0200

    Add id to ToDo model

--- cmd/model/todo.go
+++ cmd/model/todo.go
@@ -3,10 +3,12 @@ package model
 import (
 	"time"
 
+	"github.com/google/uuid"
 	"github.com/lib/pq"
 )
 
 type ToDo struct {
+	ID          uuid.UUID
 	Title       string
 	Description string
 	DueDate     time.Time
