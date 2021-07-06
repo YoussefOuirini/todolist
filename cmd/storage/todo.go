ebf0eb9962429e4c996c60084abcca83592cdea9
Youssef Ouirini <youssefouirini@gmail.com>
2021-07-06 21:03:28 +0200

    GetToDo

--- cmd/storage/todo.go
+++ cmd/storage/todo.go
@@ -1,12 +1,14 @@
 package storage
 
 import (
+	"github.com/google/uuid"
 	"github.com/jinzhu/gorm"
 	"github.com/youssefouirini/todolist/cmd/model"
 )
 
 type ToDoRepository interface {
 	CreateToDo(db *gorm.DB, toDo *model.ToDo) error
+	GetToDo(db *gorm.DB, id uuid.UUID) (*model.ToDo, error)
 }
 
 type toDoRepository struct{}
@@ -17,4 +19,12 @@ func NewToDoRepository() ToDoRepository {
 
 func (repo *toDoRepository) CreateToDo(db *gorm.DB, toDo *model.ToDo) error {
 	return db.Create(toDo).Error
+}
+
+func (repo *toDoRepository) GetToDo(db *gorm.DB, id uuid.UUID) (*model.ToDo, error) {
+	toDo := &model.ToDo{}
+
+	err := db.Model(toDo).Where("id = ?", id).Take(&toDo).Error
+
+	return toDo, err
 }
