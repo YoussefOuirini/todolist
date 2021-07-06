de61468b3695557445a681e1f882b0a1967a171e
Youssef Ouirini <youssefouirini@gmail.com>
2021-07-06 20:14:12 +0200

    Create 00001_initial.sql

---
+++ cmd/db/migrations/00001_initial.sql
@@ -0,0 +1,20 @@
+-- +goose Up
+CREATE EXTENSION IF NOT EXISTS pgcrypto SCHEMA public;
+
+CREATE TABLE todos (
+    id uuid DEFAULT gen_random_uuid() NOT NULL CONSTRAINT todos_pkey PRIMARY KEY,
+    title                 varchar(255)                      NOT NULL,
+    description           varchar(255)                      NOT NULL,
+    due_date              timestamptz                       NOT NULL,
+    is_done               BOOLEAN DEFAULT FALSE             NOT NULL,
+    labels                TEXT[],
+    created_at            timestamptz DEFAULT now()         NOT NULL,
+    updated_at            timestamptz DEFAULT now()         NOT NULL
+);
+
+;
+
+-- +goose Down
+DROP TABLE todos;
+
+;
