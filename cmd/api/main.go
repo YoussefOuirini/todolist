7daabab37d9cd123fa5691c5be14db909d5b4b08
Youssef Ouirini <youssefouirini@gmail.com>
2021-07-06 18:43:43 +0200

    add basic server

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,15 +1,64 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"net/http"
+	"strconv"
+	"time"
+)
+
+const (
+	shutdownTimeout = 10 * time.Second
+	port            = 8080
 )
 
 func main() {
-	fmt.Println("Starting server at port 8080")
+	log.Printf("Starting server at port: %v", port)
 
-	if err := http.ListenAndServe(":8080", nil); err != nil {
-		log.Fatal(err)
+	api := NewAPIServer(port)
+	err := api.ListenAndServe()
+	if err != nil {
+		log.Fatalf("http server error: %s", err)
 	}
+}
+
+type APIServer struct {
+	server *http.Server
+}
+
+func NewAPIServer(port int) *APIServer {
+	s := &APIServer{
+		server: &http.Server{
+			Addr: ":" + strconv.Itoa(port),
+		},
+	}
+
+	s.mount()
+
+	return s
+}
+
+func (s *APIServer) ListenAndServe() error {
+	return s.server.ListenAndServe()
+}
+
+func (s *APIServer) Shutdown() {
+	ctx, cancel := context.WithTimeout(context.Background(), shutdownTimeout)
+	defer cancel()
+
+	if err := s.server.Shutdown(ctx); err != nil {
+		fmt.Println("error shutting down http server gracefully")
+
+		return
+	}
+
+	fmt.Println("server shutdown gracefully")
+}
+
+func (s *APIServer) mount() {
+	http.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "yoyoyoyoyo!")
+	})
 }
