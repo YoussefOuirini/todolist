36a7dd62e65ba0bfcb8a1e215b5bdf8a47e71613
Youssef Ouirini <youssefouirini@gmail.com>
2021-07-06 21:11:05 +0200

    adding get todos

--- cmd/storage/todo.go
+++ cmd/storage/todo.go
@@ -9,6 +9,7 @@ import (
 type ToDoRepository interface {
 	CreateToDo(db *gorm.DB, toDo *model.ToDo) error
 	GetToDo(db *gorm.DB, id uuid.UUID) (*model.ToDo, error)
+	GetToDos(db *gorm.DB) ([]model.ToDo, error)
 }
 
 type toDoRepository struct{}
@@ -27,4 +28,12 @@ func (repo *toDoRepository) GetToDo(db *gorm.DB, id uuid.UUID) (*model.ToDo, err
 	err := db.Model(toDo).Where("id = ?", id).Take(&toDo).Error
 
 	return toDo, err
+}
+
+func (repo *toDoRepository) GetToDos(db *gorm.DB) ([]model.ToDo, error) {
+	var todos []model.ToDo
+
+	err := db.Model(&model.ToDo{}).Find(&todos).Error
+
+	return todos, err
 }
