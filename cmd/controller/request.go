939675ad8176f65f603b0827c73111f9ef47576c
Youssef Ouirini <youssefouirini@gmail.com>
2021-07-06 19:27:22 +0200

    adding description

--- cmd/controller/request.go
+++ cmd/controller/request.go
@@ -2,13 +2,19 @@ package controller
 
 import (
 	"errors"
+	"fmt"
 	"time"
 )
 
+const (
+	descriptionMaxLength = 200
+)
+
 type createRequest struct {
-	Title   string    `json:"title"`
-	DueDate time.Time `json:"dueDate"` //RFC 3339 format: 2021-07-12T07:20:50.52Z
-	Labels  []string  `json:"labels"`
+	Title       string    `json:"title"`
+	Description string    `json:"description"`
+	DueDate     time.Time `json:"dueDate"` //RFC 3339 format: 2021-07-12T07:20:50.52Z
+	Labels      []string  `json:"labels"`
 }
 
 func (r createRequest) validate() error {
@@ -22,6 +28,10 @@ func (r createRequest) validate() error {
 
 	if r.DueDate.Before(time.Now()) {
 		return errors.New("due date in the past")
+	}
+
+	if len(r.Description) > descriptionMaxLength {
+		return errors.New(fmt.Sprintf("description is %v, which is longer than max of %v", len(r.Description), descriptionMaxLength))
 	}
 
 	return nil
